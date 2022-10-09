7e01ab8d91d9c3b4e7d9fcd6af9a8b47e3e1394f
zw277 <112585430+zw277@users.noreply.github.com>
2022-10-09 17:12:53 +0000

    update

--- read_n.sh
+++ read_n.sh
@@ -3,7 +3,7 @@
 read_n() {
     while read line
     do
-      echo "Record is : $line"
+      echo "The Brand information is : $line"
     done < $1
 }
 read_n brandirectory-ranking-global-2022.csv
